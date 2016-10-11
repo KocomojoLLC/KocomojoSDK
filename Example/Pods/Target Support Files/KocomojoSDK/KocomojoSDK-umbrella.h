ae3de0dd2051701b465f8a1d237307688438c9f5
Elijah Windsor <ewindsor@gmail.com>
2016-10-11 14:16:34 -0400

    Added Example

---
+++ Example/Pods/Target Support Files/KocomojoSDK/KocomojoSDK-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double KocomojoSDKVersionNumber;
+FOUNDATION_EXPORT const unsigned char KocomojoSDKVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-KocomojoSDK_Tests/Pods-KocomojoSDK_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_KocomojoSDK_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_KocomojoSDK_TestsVersionString[];
+
