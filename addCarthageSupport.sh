323b1da791c5d5baabc58532f803bf2bbf91e340
Elijah Windsor <ewindsor@gmail.com>
2017-01-20 13:33:11 -0500

    added script for zipping and uploading to release

--- addCarthageSupport.sh
+++ addCarthageSupport.sh
@@ -1,2 +1,2 @@
 zip -r KocomojoSDK.framework.zip KocomojoSDK/KocomojoSDK.framework
-./upload-github-release-asset.sh github_api_token=$1 owner=KocomojoLLC repo=KocomojoSDK tag=$2 filename=./KocomojoSDK.framework.zip 
+./upload-github-release-asset.sh github_api_token=$(<.githubApiToken) owner=KocomojoLLC repo=KocomojoSDK tag=$1 filename=./KocomojoSDK.framework.zip
