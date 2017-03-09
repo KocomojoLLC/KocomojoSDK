6a3e69bc22500ac3ca6f12398bd6de42ac076bdf
Elijah Windsor <ewindsor@gmail.com>
2017-03-09 17:17:29 -0500

    Added experience.md and button.md to runAndDeploy.sh

--- runAndDeploy.sh
+++ runAndDeploy.sh
@@ -1,3 +1,6 @@
 generate-md --layout ./layout/ --input ./index.md
+generate-md --layout ./layout/ --input ./experience.md
+generate-md --layout ./layout/ --input ./button.md
+
 ( cd output && surge --domain kocomojo-sdk.surge.sh )
 
